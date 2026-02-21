3c3d5adb3b1c4654ae69ca56c87a87b6ddb23541
Aiman Mustafa <aimenmustafa1828630@gmail.com>
2026-02-21 20:51:28 +0500

    Js started

---
+++ lec-01.js
@@ -0,0 +1,37 @@
+console.log("We are learning JS");
+console.log(45);
+
+
+// data types :
+// data stored in a format
+// data types are of 4 types
+// 1- String (alphabets, written in "...")
+// 2- Integers (number without decimal)
+// 3- Double/Float (number with decimal)
+// 4- Boolean (true/false)
+
+
+
+// variables
+// variables are made by 2 keyword i.e let or const
+//format :     let variable name  = datatype;
+
+// string
+let userName = "Ali Ahmad";
+userName = "Aiman Mustafa";
+console.log(userName);
+// integer
+let userAge = 21;
+console.log(userAge);
+// double/float
+let userHeight = 5.5;
+console.log(userHeight);
+
+let isMuslim = true;
+console.log(isMuslim);
+
+// variables are written in 4 styles
+// 1- PascalCase
+// 2- camelCase. (approved)
+// 3- kebab-case
+// 4- snake_case
