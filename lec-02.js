1c6e06a3190a4dd259e5ec9a25a97ba5394c9a6c
Aiman Mustafa <aimenmustafa1828630@gmail.com>
2026-02-25 21:45:58 +0500

    lec-2

---
+++ lec-02.js
@@ -0,0 +1,46 @@
+console.log("lecture 2 of JS");
+
+
+let userName = "Ahmad Raza";
+let userAge = 34;
+
+
+console.log("His name is" , userName ,"and his age is" , userAge  );
+
+// Array : array is a set/list/collection of items.
+
+let arrayOfNames = ["maaz" , "saim" , "badar" , "ahmad" , "shahzad"];
+console.log(arrayOfNames);
+console.log(arrayOfNames[0]);
+console.log(arrayOfNames[1]);
+console.log(arrayOfNames[2]);
+console.log(arrayOfNames[3]);
+console.log(arrayOfNames[4]);
+
+
+
+// Operators : to perform functions or operation.
+// operators have 4 types
+// 1- arithmetic 2- assignment 3- comparison 4- relational*
+
+// 1- Arithmetic operators are math operators(+ , - , * , / , %)
+
+
+let a = 45;
+let b = 50; 
+// console.log(a+b);
+
+let sum = a + b;
+console.log("The sum of the given number is " , sum);
+
+let minus = a - b;
+console.log("The difference of the given number is " , minus);
+
+let multiply = a*b;
+console.log(multiply);
+
+let divide1 = a/b;
+console.log(divide1);
+
+let divide2 = a%b;
+console.log(divide2);
---
+++ revision.js
@@ -0,0 +1,54 @@
+console.log("welcome to revision of js");
+console.log(52637489);
+
+// Lecture 1
+
+// string
+let userName = "Ali Haider";
+userName = "Arsalan"
+console.log("My name is " , userName, ".");
+// integer
+const userAge = 45;
+console.log("My age is" , userAge);
+// double/float
+let userPercentage = 90.5678;
+console.log("My percentage is" ,userPercentage);
+// boolean
+let userIsMuslim = true;
+console.log(userIsMuslim);
+
+// writing case for variables
+/* 
+4 cases
+1- PascalCase
+2- camelCase
+3- snake_case
+4- kebab-case
+*/
+
+
+// Lecture 2
+
+
+// arrays
+let arrayOfUsers = ["Arsalan" , "Ahmad" , "Shahzad" , "Ahmad Ghafoor"];
+console.log(arrayOfUsers);
+console.log(arrayOfUsers[0]);
+console.log("the user name is" ,arrayOfUsers[1]);
+console.log(arrayOfUsers[2]);
+console.log(arrayOfUsers[3]);
+
+// operators
+// 1- Arithmetic(+,-,*,/,%)
+
+
+let numOne = 8797;
+let numTwo = 6789;
+let sumOfNum = numOne + numTwo;
+console.log(sumOfNum);
+
+let minusOfNum = numOne - numTwo;
+console.log(minusOfNum);
+
+let productOfNum = numOne * numTwo;
+console.log(productOfNum);
