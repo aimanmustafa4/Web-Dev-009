aca9d9561cf4ad3c39603910333281451fc92cea
Aiman Mustafa <aimenmustafa1828630@gmail.com>
2026-03-01 13:41:49 +0500

    switch-case

---
+++ practice-conditionals.js
@@ -0,0 +1,32 @@
+// let obtMarks = prompt("Enter ur marks obtained");
+// console.log("my obtain marks are :" , obtMarks);
+
+// let totalMarks = prompt("Enter ur total marks");
+// console.log("my total marks are :" , totalMarks);
+
+// let userPercentage = (obtMarks/totalMarks)*100;
+// console.log("Percentage",userPercentage);
+
+
+
+// conditional statements type 2
+// Switch-case
+
+let dayToday = prompt("What day is today ?");
+switch (dayToday) {
+    case "Monday":
+        console.log("Today is Monday");
+        break;
+    case "Tuesday":
+        console.log("Today is Tuesday");
+        break;
+    case "WED":
+        console.log("Today is WED");
+        break;
+    case "Thurs":
+        console.log("Today is Thurs");
+        break;
+    default:
+        console.log("weekend days");
+        break;
+}
