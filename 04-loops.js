81d1ff7a1ea87d1314b9bca8746639de0b82534b
Aiman Mustafa <aimenmustafa1828630@gmail.com>
2026-03-02 14:29:21 +0500

    loop lecture

---
+++ 04-loops.js
@@ -0,0 +1,36 @@
+console.log("Loops lecture");
+
+// Loop is a block of code that repeats until unless the condition is met...
+// loop is of 4 TYPES
+/*1- for loop
+2- while loop 
+3- do-while loop 
+4- foreach loop
+ */
+
+// 1- for loop 
+// in loop 3 things are described: variable, condition, upgrade
+
+// for(let i=10; i>=0; i--){
+//     console.log(i);   
+// }
+
+
+
+
+
+// 2- while loop 
+// let m = 0;
+// while(m<=5){
+//     console.log(m);
+//     m++;
+// }
+
+
+
+// 3- do-while loop
+let n = 0;
+do{
+ console.log(n);
+    n++;
+}while(n<=5);
