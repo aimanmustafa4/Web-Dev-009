abc047356eec88a2f98cba5810158bfd9eb57792
Aiman Mustafa <aimenmustafa1828630@gmail.com>
2026-03-05 13:53:23 +0500

    math function

---
+++ maths.js
@@ -0,0 +1,66 @@
+console.log("Math functions");
+
+let m = -20.3254276;
+
+// 0-5 so it either remains same or lower shift. ; 6-9 greater
+// m = Math.round(m);
+
+
+// ceil always shift towards the greater value(+1)
+// m = Math.ceil(m);
+
+
+// ceil always shift towards the lesser value(-1) or same 
+// m = Math.floor(m);
+
+// sqrt finds out the perfect square
+// m = Math.sqrt(m);
+
+// cbrt finds out the perfect cube of the value
+// m = Math.cbrt(m);
+
+
+// power given in this function
+// m = Math.pow(m,2);
+
+// absolute changes every negative value to positive
+// m = Math.abs(m)
+
+
+// m = Math.random(m)
+// console.log(m);
+
+
+
+
+let w =56;
+let x =-256;
+let y =0.76556;
+let z =38091283006;
+
+// max and min
+ let max = Math.max(m,w,x,y,z);
+ console.log(max);
+ 
+ let minimum = Math.min(m,w,x,y,z);
+ console.log(minimum);
+
+
+// simple array
+ let array = [12,321,32,3454,5345];
+ console.log(array);
+ console.log(array[2]);
+ 
+//  associative array
+// in the form of key and value pair
+
+let user= {
+    userName : "Ahmad",
+    userAge : 12,
+    userCity : "Bahawalpur",
+}
+console.log(user);
+
+console.log(user.userAge);
+console.log(user.userName);
+console.log(user.userCity);
