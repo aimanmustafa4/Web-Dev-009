9c0f91c9bf3be5b438e1875f01042cbbdb25f3b8
Aiman Mustafa <aimenmustafa1828630@gmail.com>
2026-03-11 14:39:26 +0500

    api complete

--- api.js
+++ api.js
@@ -7,21 +7,40 @@ button.addEventListener("click" , () => {
 
     let xhr = new XMLHttpRequest();
     const method = "GET";
-    const apiKey = "write ur own api key";
-    const apiEndpoint = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${apiKey}`;
+    const apiKey = "b365a2f8a0834f58b8baaef613f541e1";
+    const apiEndpoint = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${apiKey}`;
 
-    // const apiPoint = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b365a2f8a0834f58b8baaef613f541e1';
-
-    xhr.open(method, apiPoint);
+    xhr.open(method, apiEndpoint);
     xhr.onprogress = () => {
         console.log("data is in progress");
         
     };
-
     xhr.onload = () => {
         // console.log("data is loading");
         let data = xhr.responseText; 
-        console.log(data);
+        data = JSON.parse(data);
+        let articles = data.articles;
+        // console.log(articles); 
+        let newsHolder = document.querySelector(".newsHolder");
+        articles.forEach((element) => {
+            let newsBox =
+         `<div class="news-card">
+            <div class="news-card-image">
+                <img src="${element.urlToImage}" alt="News">
+            </div>
+            <div class="news-card-content">
+                <h2>${element.title}</h2>
+                <p class="news-card-source">${element.source}</p>
+                <p class="news-card-source">${element.author}</p>
+                <p class="news-card-description">${element.description}</p>
+                <a href="${element.url}" class="news-card-link">Read more →</a>
+            </div>
+        </div>`
+
+        newsHolder.innerHTML += newsBox
+        });
+ 
+        
                
     };
 
